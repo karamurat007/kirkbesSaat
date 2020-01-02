1d4804fffb7ef5d6e5bb788eb18d6d2d9eb364d2
Ayhan Bahadir Meric <bahadirmeric@outlook.com>
2020-01-02 22:00:25 +0300

    Dama TAhtası genel hatlarıyla oluşturuldu

--- Classlar/WindowsFormsApp1/Form1.cs
+++ Classlar/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,27 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] buttons = new Button[8, 8];
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    this.Controls.Add(buttons[i, j]);
+                    left += 50;
+                }
+                top += 50;
+                left = 0;
+            }
+        }
     }
 }
